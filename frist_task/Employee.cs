a5831a76fbb9986b5a7bdb446f5df64fda1cbb7c
Abdalrahman Tarek <abdalrahmantarek34@gmail.com>
2023-03-18 13:09:31 +0200

    Add project files.

---
+++ frist_task/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System.Security.AccessControl;
+using System.Xml.Linq;
+
+namespace frist_task
+{
+    class Employee
+    {
+
+        protected readonly decimal overTimeRate=1.25m;
+        protected  decimal overTimeHours;
+        protected const decimal minHours = 176m;
+
+        public Employee(int id, string? name, decimal loggedHours, decimal wage)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.LoggedHours = loggedHours;
+            this.Wage = wage;
+        }
+
+        protected int Id { get; set; }
+        protected string? Name { get; set; }
+        protected decimal LoggedHours { get; set; }
+        protected decimal Wage { get; set; }
+       
+        protected virtual decimal BasicSalary() 
+        {
+
+            return minHours * Wage  ;
+        }
+        protected virtual decimal OverTime()
+        {
+            overTimeHours = (LoggedHours - minHours);
+            var overTimeTotalPrice = overTimeHours *Wage * overTimeRate;
+            return overTimeHours > 0 ? overTimeTotalPrice : 0 ;
+        }
+        protected virtual decimal MonthlySalary() 
+        {
+            return BasicSalary () +OverTime() ;
+        }
+        public override string ToString()
+        {
+            return $"{GetType ().ToString().Replace("frist_task.","")}"+
+                   $"\nid:{Id}" +
+                   $"\nname:{Name}" +
+                   $"\nloged hours:{LoggedHours}" +
+                   $"\nWage:{Wage}" +
+                   $"\nover Time:{Math.Round( OverTime(),2)}" +
+                   $"\nBasic salary:{Math.Round( BasicSalary(),2)}";
+        }
+        
+
+    }
+
+}
+
---
+++ frist_task/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Security.Principal;
+
+namespace frist_task
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Manager m = new Manager(1, "ali", 176, 10);
+
+
+            Maintenancy maintenancy = new Maintenancy(1, "ali", 176, 10);
+
+            Devoloper devoloper = new Devoloper(1, "abdo", 180, 14, 0.05m, true);
+
+            Sales sales = new Sales(1, "Mohamed", 176, 8,0.05m,10_000);
+         
+            Employee[] employee = { m, maintenancy, sales, devoloper };
+            foreach (Employee e in employee) 
+            {
+                Console.WriteLine("\n----------------");
+                Console.WriteLine(e);
+
+            }
+        }
+
+    }
+   
+}
+
